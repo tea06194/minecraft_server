a6dbbab4c226558941d2894f8bbb45ab756e5936
tea06194 <tea06194@gmail.com>
2025-08-11 01:30:33 +0300

    in continuation service

--- curse-services/get-mods-by.js
+++ curse-services/get-mods-by.js
@@ -6,10 +6,42 @@ const headers = {
 };
 
 const methods = {
-	games: fetch(`${BASE_URL}/games`, {
-		headers: headers,
-		method: "GET",
-	})
+	async getGames() {
+		const res = await fetch(`${BASE_URL}/games`, {
+			headers: headers,
+			method: "GET",
+		});
+		return await res.json();
+	},
+	async getMinecraftGameId() {
+		const games = await this.getGames();
+		const minecraftGame = games.data.find(game => game.slug === "minecraft");
+
+		if (!minecraftGame) {
+			throw Error("Minecraft game not found")
+		}
+
+		return minecraftGame.id;
+	},
+	async searchModsMinecraft() {
+		const url = new URL(`${BASE_URL}/mods/search`)
+
+		const gameId = await this.getMinecraftGameId()
+		url.searchParams.append("gameId", gameId)
+
+		console.log(url)
+
+		const res = await fetch(url.href, {
+			method: 'GET',
+			headers: headers
+		})
+
+		const data = await res.json()
+
+		return data
+	}
 }
 
-methods.games.then(res => res.json()).then(console.log)
+methods.searchModsMinecraft()
+	.then(data => console.log("Success:", data))
+	.catch(error => console.error("Error:", error));
