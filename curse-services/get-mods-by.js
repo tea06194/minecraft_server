8a9b7a8b9998007fef35dc5af953865611c7ddda
tea06194 <tea06194@gmail.com>
2025-08-12 02:03:32 +0300

    in contin serv (probaply not)

--- curse-services/get-mods-by.js
+++ curse-services/get-mods-by.js
@@ -1,8 +1,18 @@
 const BASE_URL = "https://api.curseforge.com/v1";
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.API_KEY;
 const headers = {
 	Accept: "application/json",
 	"x-api-key": `${API_KEY}`,
+};
+
+const modLoaderTypes = {
+	"Any": 0,
+	"Forge": 1,
+	"Cauldron": 2,
+	"LiteLoader": 3,
+	"Fabric": 4,
+	"Quilt": 5,
+	"NeoForge": 6,
 };
 
 const methods = {
@@ -18,30 +28,96 @@ const methods = {
 		const minecraftGame = games.data.find(game => game.slug === "minecraft");
 
 		if (!minecraftGame) {
-			throw Error("Minecraft game not found")
+			throw Error("Minecraft game not found");
 		}
 
 		return minecraftGame.id;
 	},
-	async searchModsMinecraft() {
-		const url = new URL(`${BASE_URL}/mods/search`)
+	async searchModsMinecraftForge(modsSearchData) {
 
-		const gameId = await this.getMinecraftGameId()
-		url.searchParams.append("gameId", gameId)
+		const gameId = await this.getMinecraftGameId();
 
-		console.log(url)
+		const modsIdsPromises = modsSearchData.slugs.map((modName) => {
+			const url = new URL(`${BASE_URL}/mods/search`);
+			const searchParams = new URLSearchParams({
+				gameId,
+				modLoaderType: modLoaderTypes.Forge,
+				gameVersion: modsSearchData.gameVersion,
+				slug: modName.toLowerCase()
+			});
+
+			url.search = searchParams.toString();
+
+			return fetch(url.href, {
+				method: 'GET',
+				headers: headers
+			}).then((res) => {
+				return res.json();
+			});
+		});
+
+		const modsIds = await Promise.all(modsIdsPromises);
+
+		modsIds.map(data => console.log(data.data[0]?.slug))
+		/* const searchParams = new URLSearchParams({
+			slug: modsSearchData.slugs[0].toLowerCase()
+		});
+
+
+		url.search = searchParams.toString();
+
 
 		const res = await fetch(url.href, {
 			method: 'GET',
 			headers: headers
-		})
+		});
 
-		const data = await res.json()
+		const data = await res.json();
 
-		return data
+		return data; */
 	}
-}
+};
+let data;
 
-methods.searchModsMinecraft()
-	.then(data => console.log("Success:", data))
+const modsSearchData = {
+	gameVersion: "1.20.1",
+	slugs: [
+		"Croptopia",
+		"Steam_Rails",
+		"ad_astra-forge",
+		"advanced-xray-forge",
+		"architectury",
+		"botarium-forge",
+		"canary",
+		"create",
+		"create_structures_arise",
+		"createbigcannons",
+		"createdeco",
+		"ferritecore",
+		"ftb-chunks-forge",
+		"ftb-essentials-forge",
+		"ftb-library-forge",
+		"ftb-teams-forge",
+		"ftbbackups2-forge",
+		"gravestone-forge",
+		"jei",
+		"journeymap",
+		"kotlinforforge",
+		"modernfix-forge",
+		"polylib-forge",
+		"resourcefulconfig-forge",
+		"resourcefullib-forge",
+		"starlight",
+		"tfmg",
+	]
+};
+
+methods.searchModsMinecraftForge(modsSearchData)
+	.then(data1 => {
+		console.log("Success:", data1);
+		data = data1;
+		console.log("🪵AVA data", data, "AVA");
+	})
 	.catch(error => console.error("Error:", error));
+
+console.log("end")
